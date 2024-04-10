3f12e9610f8d305a92bba364addb05b4d3769720
LeivaDiego <lei21752@uvg.edu.gt>
2024-04-09 20:46:48 -0600

    Improved Node Creation with Error Handling

--- main.py
+++ main.py
@@ -11,10 +11,33 @@ if not load_status:
 URI = os.getenv('NEO4J_URI')
 AUTH = (os.getenv('NEO4J_USERNAME'), os.getenv('NEO4J_PASSWORD'))
 
+
+def create_node(tx, labels, properties):
+    if labels is None:
+        raise ValueError('Labels not provided')
+    
+    if properties is None:
+        raise ValueError('Properties not provided')
+    
+    query = f"""
+        CREATE (n:{':'.join(labels)} $properties)
+        RETURN n
+        """
+    result = tx.run(query, properties=properties)
+    print(f'SUCCES: Node with label: {labels} and properties: {properties} created\n') 
+    return result.consume()
+
+
 try:
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
         driver.verify_connectivity()
-        print('Successfully connected to Neo4j Instance')
+        print('SUCCES: Connection to Neo4j instance established\n')
+        with driver.session() as session:
+            # Prueba de creacion de nodo
+            result = create_node(session, ['Person'], {'name': 'Alice', 'age': 25})
 
 except Exception as e:
-    print(f'Error: {e}')
+    print(f'ERROR: {e}')
+
+finally:
+    driver.close()
